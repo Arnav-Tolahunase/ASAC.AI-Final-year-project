ce163ce68109ecc0319df966da9b8b6b3c99bb09
ARNAV <arnav.tolahunase21@gmail.com>
2026-06-12 13:00:23 +0530

    Move frontend app and clean build artifacts

--- .next/server/pages/_app.js
+++
@@ -1,5 +0,0 @@
-const CHUNK_PUBLIC_PATH = "server/pages/_app.js";
-const runtime = require("../chunks/ssr/[turbopack]_runtime.js");
-runtime.loadChunk("server/chunks/ssr/[root-of-the-server]__5b0ec742._.js");
-runtime.getOrInstantiateRuntimeModule("[project]/ASAC-AI/remix-of-asacai-media-integrity-ui/node_modules/next/app.js [ssr] (ecmascript)", CHUNK_PUBLIC_PATH);
-module.exports = runtime.getOrInstantiateRuntimeModule("[project]/ASAC-AI/remix-of-asacai-media-integrity-ui/node_modules/next/app.js [ssr] (ecmascript)", CHUNK_PUBLIC_PATH).exports;
--- .next/static/chunks/ASAC-AI_remix-of-asacai-media-integrity-ui_src_app_icon_svg_mjs_7454abe7._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/3e5b4_next_dist_52b69222._.js"
-],
-    source: "dynamic"
-});
